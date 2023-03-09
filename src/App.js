25633cb47c2322bd10964f6d636cfb6a4a788c25
marcinoles87 <marcinoles87@gmail.com>
2023-03-09 20:31:55 +0100

    build structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
+import Navbar from './components/Navbar';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './navbar.css'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className='nav-wrapper'>
+        <div className='nav-logo'>
+            <h1>Name</h1>
+            <img className='nav-logo-img' src='#' alt='logo'></img>
+        </div>
+
+        <div className='nav-menu'>
+            
+
+        </div>
+
+        </nav>
+    )
+  }
+}
