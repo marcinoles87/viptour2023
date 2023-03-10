899831d055060a9a1f03a08307769546137a3168
marcinoles87 <marcinoles87@gmail.com>
2023-03-10 20:41:04 +0100

    add json data

---
+++ src/components/Trips.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Trips extends Component {
+  render() {
+    return (
+      <div className='trips-wrapper'>Trips
+        <div className='trips-card'></div>
+      </div>
+    )
+  }
+}
