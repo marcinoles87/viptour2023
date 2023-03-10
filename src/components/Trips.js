f419db157d86a1a4841c78371c1807b0b23cecef
marcinoles87 <marcinoles87@gmail.com>
2023-03-10 20:42:48 +0100

    add json data

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
+import JSONDATA from '../data/data.json'
+
 export default class Trips extends Component {
   render() {
     return (
       <div className='trips-wrapper'>Trips
-        <div className='trips-card'></div>
+        <div className='trips-card'>
+            {JSONDATA.map( (item , index) => {
+                return <ul>
+                    <li>{item.name}</li>
+                </ul>
+            })}
+        </div>
       </div>
     )
   }
