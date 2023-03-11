87561dc2f66f6f3be36654c3477cad5f05654b38
marcinoles87 <marcinoles87@gmail.com>
2023-03-11 14:43:22 +0100

    work with json

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -8,9 +8,12 @@ export default class Trips extends Component {
       <div className='trips-wrapper'>Trips
         <div className='trips-card'>
             {JSONDATA.map( (item , index) => {
-                return <ul className={item.cName}>
+                return <ul className={item.cName} key={item.index}>
                     <h1>{item.name}</h1>
                     <img src={item.img}></img>
+                    <h3> Trip time :</h3>
+                    <h4> Persone : </h4>
+                    <h4> Price :</h4>
                 </ul>
             })}
         </div>
