1d41253318b933f3b6840b82a1eaa41c5da1b5c3
marcinoles87 <marcinoles87@gmail.com>
2023-03-11 15:19:03 +0100

    work with media

--- src/components/News.js
+++ src/components/News.js
@@ -7,8 +7,14 @@ export default class News extends Component {
     return (
       <div className='news-wrapper'>
         <div className='news-info'>
-            <h1>Title</h1>
-                <p>Information</p>
+            <h1>Before We Drive</h1>
+                <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w 
+                  przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego
+                   drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być 
+                   używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował s
+                   ię w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem 
+                   Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do 
+                   realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
                 <a href='#'>Link</a>
                 
         </div>
--- src/components/Trips.js
+++ src/components/Trips.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react'
 
 import JSONDATA from '../data/data.json'
 
+import './trips.scss'
+
 export default class Trips extends Component {
   render() {
     return (
-      <div className='trips-wrapper'>Trips
+      <div className='trips-wrapper'>
+        <h1>Trips</h1>
         <div className='trips-card'>
             {JSONDATA.map( (item , index) => {
                 return <ul className={item.cName} key={item.index}>
+                  
                     <h1>{item.name}</h1>
-                    <img src={item.img}></img>
+                    <img src={item.img} alt='trips'></img>
                     <h3> Trip time : </h3>
                     <h4> Persone :{item.persone} </h4>
                     <h4> Price :</h4>
+                  
                 </ul>
             })}
         </div>
