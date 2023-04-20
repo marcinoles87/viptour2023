e90b5e2b7d7114ed5e86e1b10728582d16cafb3a
marcinoles87 <marcinoles87@gmail.com>
2023-04-20 18:41:53 +0200

    add new function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import News from './components/News';
@@ -7,14 +7,35 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 import './App.css';
+import DATA from './data.json'
+
+
 
 function App() {
+
+  console.log(DATA)
+
+let [datas , setData] = useState([])
+
+const handleData = () => {
+  setData(
+    datas=DATA
+  )
+}
+
+
+
   return (
     <div className="App">
       <Navbar></Navbar>
       <Header></Header>
       <News></News>
-      <Trips></Trips>
+
+      {datas.map( (data,index) => {
+        
+        <Trips></Trips>
+      })}
+      
       <Contact></Contact>
       <Footer></Footer>
     </div>
